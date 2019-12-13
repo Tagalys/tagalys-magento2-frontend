8a43ae1a99bbb3fdffd71263ab29b43269387131
Palaniappan Chellappan <palaniappanc@gmail.com>
2019-12-13 17:35:49 +0530

    misc updates

--- Block/Tsearch.php
+++ Block/Tsearch.php
@@ -5,14 +5,8 @@ class Tsearch extends \Magento\Framework\View\Element\Template
 {
     public function __construct(
       \Magento\Framework\View\Element\Template\Context $context
-      // \Tagalys\Sync\Helper\Configuration $configurationHelper
     )
     {
-        // $this->productHelper = $productHelper;
-        // $this->configurationHelper = $configurationHelper;
-        // $this->request = $request;
-        // $this->configCollectionFactory = $configCollectionFactory;
-        // $this->configFactory = $configFactory;
         parent::__construct($context);
     }
 
