86d9129edee4a2a7c5352056ed0d130bc630eb4d
Shankar T <shankar@tagalys.com>
2020-10-13 01:06:49 +0530

    redirect to homepage when no search query is given

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -13,16 +13,23 @@ class Index extends \Magento\Framework\App\Action\Action
      */
     private $tagalysConfiguration;
 
+    /**
+     * @param \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
     public function __construct(
         Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
         \Magento\Framework\View\Page\Config $pageConfig,
-        \Tagalys\Sync\Helper\Configuration $tagalysConfiguration
+        \Tagalys\Sync\Helper\Configuration $tagalysConfiguration,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     )
     {
         $this->_resultPageFactory = $resultPageFactory;
         $this->pageConfig = $pageConfig;
         $this->tagalysConfiguration = $tagalysConfiguration;
+        $this->storeManager = $storeManager;
         $this->request = $context->getRequest();
         parent::__construct($context);
     }
@@ -32,6 +39,14 @@ class Index extends \Magento\Framework\App\Action\Action
         $resultPage = $this->_resultPageFactory->create();
 
         $params = $this->request->getParams();
+
+        if(!array_key_exists('q', $params)) {
+            $searchFallbackUrl = $this->tagalysConfiguration->getConfig('search:fallback_url');
+            if($searchFallbackUrl == null) {
+                $searchFallbackUrl = $this->storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_WEB, true);
+            }
+            $this->_redirect($searchFallbackUrl);
+        }
         $resultPage->getConfig()->getTitle()->set('Search Results for "' . $params['q'] . '"');
 
         $this->pageConfig->setRobots('NOINDEX,NOFOLLOW');
